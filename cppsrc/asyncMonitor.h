47ed4f89c7ab02afdcb7f537cd5620e63549f3ce
Woof <gchen1258@gmail.com>
2019-06-04 12:19:25 -0400

    Added in error checking for some functions

--- cppsrc/asyncMonitor.h
+++ cppsrc/asyncMonitor.h
@@ -4,19 +4,19 @@
 
 namespace ShopifyAsync
 {
-Napi::Value FindProductByTitleAsync(const Napi::CallbackInfo &info);
-Napi::Value FindProductByHandleAsync(const Napi::CallbackInfo &info);
-Napi::Value GetAllProductsAsync(const Napi::CallbackInfo &info);
-Napi::Value GetAllCleanedAsync(const Napi::CallbackInfo &info);
-Napi::Value InstockSizesAsync(const Napi::CallbackInfo &info);
-Napi::Value OutSizesAsync(const Napi::CallbackInfo &info);
-Napi::Value GetSizesAsync(const Napi::CallbackInfo &info);
-Napi::Value CheckRestocked(const Napi::CallbackInfo &info);
-Napi::Value SearchProductByTitleAsync(const Napi::CallbackInfo &info);
-Napi::Value SearchProductByHandleAsync(const Napi::CallbackInfo &info);
-Napi::Value SearchInstockSizesAsync(const Napi::CallbackInfo &info);
-Napi::Value SearchOutSizesAsync(const Napi::CallbackInfo &info);
-Napi::Value SearchSizesAsync(const Napi::CallbackInfo &info);
+Napi::Value FindProductByTitleAsync(const Napi::CallbackInfo &info);//
+Napi::Value FindProductByHandleAsync(const Napi::CallbackInfo &info);//
+Napi::Value GetAllProductsAsync(const Napi::CallbackInfo &info);//
+Napi::Value GetAllCleanedAsync(const Napi::CallbackInfo &info);//
+Napi::Value InstockSizesAsync(const Napi::CallbackInfo &info);//
+Napi::Value OutSizesAsync(const Napi::CallbackInfo &info);//
+Napi::Value GetSizesAsync(const Napi::CallbackInfo &info);//
+Napi::Value CheckRestocked(const Napi::CallbackInfo &info);//
+Napi::Value SearchProductByTitleAsync(const Napi::CallbackInfo &info);//
+Napi::Value SearchProductByHandleAsync(const Napi::CallbackInfo &info);//
+Napi::Value SearchInstockSizesAsync(const Napi::CallbackInfo &info);//
+Napi::Value SearchOutSizesAsync(const Napi::CallbackInfo &info);//
+Napi::Value SearchSizesAsync(const Napi::CallbackInfo &info);//
 Napi::Object Init(Napi::Env env, Napi::Object exports);
 } // namespace ShopifyAsync
 
